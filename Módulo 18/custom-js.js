96e92bf79bb00d7fcad38289334128541a5ab041
lucasmrodrigo <rodrigo.lm2904@gmail.com>
2022-10-19 14:40:48 -0300

    Manipulação com jQuery
    Importado código fonte passado em aula; pequenas alterações utilizando seletores através do jQuery; importado bootstrap para o projeto.

--- Módulo 18/custom-js.js
+++ Módulo 18/custom-js.js
@@ -1,7 +1,12 @@
-jQuery(function($){
+//jQuery(function($){
 
-    $('h4').first().text('Somente título 1 alterado');
-    $('.featured-item').addClass('classe-alterada');
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>');
 
 })
 
