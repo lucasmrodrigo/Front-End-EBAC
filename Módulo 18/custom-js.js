16d7d90af5385561ca3875b5288734bc3a1bf799
lucasmrodrigo <rodrigo.lm2904@gmail.com>
2022-10-20 22:01:30 -0300

    Copiado código fonte do professor
    Copiados códigos fonte CSS e JavaScript da OWL, index.html e style.css para efetuar pequenas mudanças e deixar o site com o perfil desejado de um e-commerce de tênis.
    Commit realizado apenas para a visualização das cópias realizadas.

--- Módulo 18/custom-js.js
+++ Módulo 18/custom-js.js
@@ -6,7 +6,17 @@ $(document).ready(function(){
 
     $('.featured-item a').addClass('btn btn-dark stretch-link');
 
-    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>');
+    //$('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>');
+
+    $('.featured-item h4').mouseenter(function(){
+        
+        $(this).append('<span class="badge bg-secondary">Novo</span>');
+        $(this).css({
+            'color': 'black',
+            'background': '#F08080'
+        })
+    })
+       
 
 })
 
