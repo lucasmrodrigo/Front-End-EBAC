af6dd3d636253b48117627b56b568697a8d86708
lucasmrodrigo <rodrigo.lm2904@gmail.com>
2022-11-15 20:46:46 -0300

    Alterado código index para localização da pasta do jQuery
    Manutenção do código no arquivo index para localização da pasta jQuery e envio do arquivo conforme solicitado pela professora.

--- Módulo 19/custom-index.js
+++ Módulo 19/custom-index.js
@@ -20,4 +20,20 @@ $(document).ready(function(){
 
     })
 
+    $('nav-modal-open').on('click', function(e){
+
+        e.preventDefault();
+
+        let elem = $(this).attr('rel')
+
+        $('.modal-body').html($('#'+elem).html())
+
+        $('.modal-header h5.modal-title').html($(this).text())
+
+        let myModal = new bootstrap.Modal($('#modelId'))
+
+        myModal.show();
+
+    })
+
 })
